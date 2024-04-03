eb60e96dbda22c207b5876ffdf3134c03134e8ed
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2024-04-03 19:05:22 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    watchfont.showSorobanNumber(angle8.input12(0), 0, 5)
+    angle8.setRGB(
+    0,
+    255,
+    0,
+    0,
+    angle8.input8(0)
+    )
+    basic.pause(100)
+})
